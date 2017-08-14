5cbc5df911da1997315b311179a98af4bcbef7b0
johndeguise <degui1jr@cmich.edu>
2017-08-14 19:50:42 -0400

    Initial commit;  initial stages of CRUD interface

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by goose on 8/14/17.
+//  Copyright © 2017 goose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Our Default Text"
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+}
+
